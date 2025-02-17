b307783b2a6ebbb87d990f6520775a702d0d27e2
Matteo <116072651+rimaout@users.noreply.github.com>
2025-02-17 14:59:58 +0100

    updated pagetitle component

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -4,11 +4,11 @@ import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
-  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title    
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <img src="/static/icon.png" alt="icon" class="icon" />   // added (icon + use you file path here)
+      <img src="/static/icon.png" alt="icon" class="icon" />
       <a href={baseDir}>{title}</a>
     </h2>
   )
@@ -16,18 +16,42 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
 
 PageTitle.css = `
 .page-title {
-  font-size: 2.2rem;         //modified (increased font)
+  font-size: 2.2rem;
   margin: 0;
-  display: flex;             //added
-  align-items: center;       //added
-  justify-content: center;   //added
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
-.page-title .icon {          //added 
-  margin-right: 0.5rem;      //added (right padding) 
-  width: 60px;               //added (icon size - change to fit your needs)
-  height: 60px;              //added (icon size - change to fit your needs)
-}                            //added
+.page-title .icon {
+  margin-right: 0.5rem;
+  width: 60px;
+  height: 60px;
+}
 `
 
 export default (() => PageTitle) satisfies QuartzComponentConstructor
+
+/* ORIGINAL
+
+import { pathToRoot } from "../util/path"
+import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
+import { classNames } from "../util/lang"
+import { i18n } from "../i18n"
+
+const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
+  const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
+  const baseDir = pathToRoot(fileData.slug!)
+  return (
+    <h2 class={classNames(displayClass, "page-title")}>
+      <a href={baseDir}>{title}</a>
+    </h2>
+  )
+}
+
+PageTitle.css = `
+.page-title {
+  font-size: 1.75rem;
+  margin: 0;
+}
+*/
